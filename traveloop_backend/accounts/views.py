c16d804e3770769ca858c703fa31649997a1037d
joshimorar <morardasjoshi@gmail.com>
2026-05-10 17:14:29 +0530

    I have added the accounts for travel backend

--- traveloop_backend/accounts/views.py
+++ traveloop_backend/accounts/views.py
@@ -3,8 +3,8 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
-from django.contrib.auth import authenticate
-from .serializers import RegisterSerializer, UserSerializer
+from django.contrib.auth import authenticate 
+from .serializers import RegisterSerializer, UserSerializer #Register serializer
 
 class RegisterView(generics.CreateAPIView):
     serializer_class = RegisterSerializer
